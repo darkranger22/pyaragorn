0d1ca312ba499b549bc756353387118532294680
Martin Larralde <martin.larralde@embl.de>
2025-05-27 01:13:15 +0200

    Drop walrus operator from `pyaragorn.tests` for Python 3.7 compatibility

--- src/pyaragorn/tests/test_rna_finder.py
+++ src/pyaragorn/tests/test_rna_finder.py
@@ -14,7 +14,7 @@ def batched(iterable, n, *, strict=False):
     if n < 1:
         raise ValueError('n must be at least one')
     iterator = iter(iterable)
-    while batch := tuple(itertools.islice(iterator, n)):
+    for batch in iter(lambda: tuple(itertools.islice(iterator, n)), ()):
         if strict and len(batch) != n:
             raise ValueError('batched(): incomplete batch')
         yield batch
